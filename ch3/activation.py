deb845b6d49e23a9cadfa6f3129fbedc16e7b3f8
sckimynwa <tsb03672@naver.com>
2021-10-09 22:56:44 +0900

    feat: add datasets

--- ch3/activation.py
+++ ch3/activation.py
@@ -15,6 +15,15 @@ def relu(x):
     return np.maximum(0, x)
 
 
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a-c)  # prevent overflow
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+
 def draw(x, y):
     plt.plot(x, y)
     plt.ylim(-0.1, 1.1)
