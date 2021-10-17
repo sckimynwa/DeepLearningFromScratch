1f18be0fbd59de2cad853240b51ca487cfab9a81
sckimynwa <tsb03672@naver.com>
2021-10-17 09:22:46 +0900

    feat: add three_layer_net

--- ch4/train_neuralnet.py
+++ ch4/train_neuralnet.py
@@ -5,11 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
 from two_layer_net import TwoLayerNet
+from three_layer_net import ThreeLayerNet
 
 # 데이터 읽기
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+# network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+network = ThreeLayerNet(input_size=784, hidden_size_1=20, hidden_size_2=20, output_size=10)
 
 # 하이퍼파라미터
 iters_num = 10000  # 반복 횟수를 적절히 설정한다.
@@ -31,11 +33,12 @@ for i in range(iters_num):
     t_batch = t_train[batch_mask]
     
     # 기울기 계산
-    #grad = network.numerical_gradient(x_batch, t_batch)
-    grad = network.gradient(x_batch, t_batch)
+    grad = network.numerical_gradient(x_batch, t_batch)
+    # grad = network.gradient(x_batch, t_batch)
     
     # 매개변수 갱신
-    for key in ('W1', 'b1', 'W2', 'b2'):
+    # for key in ('W1', 'b1', 'W2', 'b2'):
+    for key in ('W1', 'b1', 'W2', 'b2', 'W3', 'b3'):
         network.params[key] -= learning_rate * grad[key]
     
     # 학습 경과 기록
