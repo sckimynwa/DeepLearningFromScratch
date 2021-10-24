bbb935b1a68cac96649bb7589b0521972a6c2db2
sckimynwa <tsb03672@naver.com>
2021-10-24 21:10:40 +0900

    feat: add sigmoid & affine

--- ch5/layer_naive.py
+++ ch5/layer_naive.py
@@ -1,3 +1,4 @@
+import numpy as np
 class MulLayer:
   def __init__(self):
     self.x = None
@@ -28,6 +29,61 @@ class AddLayer:
     dy = dout * 1
     return dx, dy
 
+class Relu:
+  def __init__(self):
+    self.mask = None
+  
+  def forward(self, x):
+    self.mask = (x <= 0)
+    out = x.copy()
+    out[self.mask] = 0
+
+    return out
+  
+  def backward(self, dout):
+    dout[self.mask] = 0
+    dx = dout
+
+    return dx
+
+class Sigmoid:
+  def __init__(self):
+    self.out = None
+  
+  def forward(self, x):
+    out = 1 / (1 + np.exp(-x))
+    self.out = out
+
+    return out
+
+  def backward(self, dout):
+    y = self.out
+    dx = dout * y * (1.0 - y)
+
+    return dx
+
+
+class Affine:
+  def __init__(self, W, b):
+    self.W = W
+    self.b = b
+    self.x = None
+    self.dW = None
+    self.db = None
+
+  def forward(self, x):
+    self.x = x
+    out = np.dot(x, self.W) + self.b
+
+    return out
+
+  def backward(self, dout):
+    dx = np.dot(dout, self.W.T)
+    self.dw = np.dot(self.x.T, dout)
+    self.db = np.sum(dout, axis = 0)
+
+    return dx
+
 # Examples
 apple = 100
 apple_num = 2
@@ -47,7 +103,9 @@ orange_price = mul_orange_layer.forward(orange, orange_num)
 all_price = add_apple_orange_layer.forward(apple_price, orange_price)
 price = mul_tax_layer.forward(all_price, tax)
 
+print("##### Forward Propagation Result #####")
 print(price)
+print('\n')
 
 # Backward Propagation
 dprice = 1
@@ -56,4 +114,16 @@ dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)
 dapple, dapple_num = mul_apple_layer.backward(dapple_price)
 dorange, dorange_num = mul_orange_layer.backward(dorange_price)
 
+print("##### Backward Propagation Result #####")
 print(dprice, dall_price, dtax, dapple_price, dorange_price, dapple_price, dapple_num, dorange_price, dorange_num)
+print('\n')
+
+print("##### Relu Layer #####")
+x = np.array([[1.0, -0.5], [-2.0, 3.0]])
+print("original array")
+print(x)
+print('\n')
+mask = (x <= 0)
+print("mask array")
+print(mask)
+print('\n')
