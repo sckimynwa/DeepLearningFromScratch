6487f03a1b7b10295ffd3f54be5cc995ae7b223e
sckimynwa <tsb03672@naver.com>
2021-10-31 15:17:38 +0900

    feat: add convolution

---
+++ ch7/cnn.py
@@ -0,0 +1,27 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from common.util import im2col, col2im
+
+class Convolution:
+  def __init__(self, W, b, stride=1, pad=0):
+    self.W = W
+    self.b = b
+    self.stride = stride
+    self.pad = pad
+
+  def forward(self, x):
+      FN, C, FH, FW = self.W.shape
+      N, C, H, W = x.shape
+      out_h = int(1 + (H + 2*self.pad - FH) / self.stride)
+      out_w = int(1 + (W + 2*self.pad - FW) / self.stride)
+
+      col = im2col(x, FH, FW, self.stride, self.pad) # (N*out_H*out_W, C*FH*FW)
+      col_W = self.W.reshape(FN, -1).T # (C*FH*FW, FN)
+      out = np.dot(col, col_W) + self.b # (N*out_H*out_W, FN)
+
+      out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+      # (N, out_h, out_w, FN) -> (N, FN, out_h, out_w)
+
+      return out
+
