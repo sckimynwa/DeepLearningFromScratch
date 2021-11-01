2abb7eb372857a09690393f224610403768b3cec
sckimynwa <tsb03672@naver.com>
2021-11-01 23:47:19 +0900

    feat: add additional class

--- Book2/ch1/forward_net.py
+++ Book2/ch1/forward_net.py
@@ -1,21 +1,104 @@
 import numpy as np
 
+
+def softmax(a):
+  c = np.max(a)
+  exp_a = np.exp(a-c)
+  sum_exp_a = np.sum(exp_a)
+  y = exp_a / sum_exp_a
+
+  return y
+
+def cross_entropy_error(y, t):
+  if y.ndim == 1:
+    t = t.reshape(1, t.size)
+    y = y.reshape(1, y.size)
+    
+  batch_size = y.shape[0]
+  return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
+
 class Sigmoid:
   def __init__(self):
     self.params = []
+    self.out = None
   
   def forward(self, x):
-    return 1 / (1 + np.exp(-x))
+    out = 1 / (1 + np.exp(-x))
+    self.out = out
+    return out
+
+  def backward(self, dout):
+    dx = dout * (1.0 - self.out) * self.out
+    return dx
   
 class Affine:
   def __init__(self, W, b):
     self.params = [W, b]
+    self.grads = [np.zeros_like(W), np.zeros_like(b)]
+    self.x = None
   
   def forward(self, x):
     W, b = self.params
     out = np.matmul(x, W) + b
-    
+    self.x = x
     return out
+
+  def backward(self, dout):
+    W, b = self.params
+    dx = np.matmul(dout, W.T)
+    dW = np.matmul(self.x.T, dout)
+    db = np.sum(dout, axis=0)
+
+    self.grads[0][...] = dW
+    self.grads[1][...] = db
+    return dx
+
+class MatMul:
+  def __init__(self, W):
+    self.params = [W]
+    self.grads = [np.zeros_like(W)]
+    self.x = None
+  
+  def forward(self, x):
+    W, = self.params
+    out = np.matmul(x, W)
+    self.x = x
+    return out
+  
+  def backward(self, dout):
+    W, = self.params
+    dx = np.matmul(dout, W.T)
+    dW = np.matmul(self.x.T, dout)
+    self.grads[0][...] = dW
+    return dx
+
+class SoftmaxWithLoss:
+  def __init__(self):
+    self.loss = None
+    self.y = None
+    self.t = None
+
+  def forward(self, x, t):
+    self.t = t
+    self.y = softmax(x)
+    self.loss = cross_entropy_error(self.y, self.t)
+    
+    return self.loss
+  
+  def backward(self, dout):
+    batch_size = self.t.shape[0]
+    dx = (self.y - self.t) / batch_size
+
+    return dx
+
+class SGD:
+  def __init__(self, lr=0.01):
+    self.lr = lr
+  
+  def update(self, params, grads):
+    for i in range(len(params)):
+      params[i] -= self.lr * grads[i]
 
 class TwoLayerNet:
   def __init__(self, input_size, hidden_size, output_size):
