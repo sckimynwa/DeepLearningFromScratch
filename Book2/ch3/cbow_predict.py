0937a7bcd6288fbbc44fcc5e90ad0030e8199b3a
sckimynwa <tsb03672@naver.com>
2021-11-06 09:41:19 +0900

    feat: cbow preprocessing

---
+++ Book2/ch3/cbow_predict.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append('..')
+from common.util import preprocess, create_contexts_target, convert_one_hot
+
+# Preprecess Text
+text = 'You say goodbye and I say hello.'
+corpus, word_to_id, id_to_word = preprocess(text)
+
+print('===== Word Preprocess =====')
+print(corpus)
+print(word_to_id)
+print(id_to_word)
+print('\n')
+
+print('===== Context and Targets =====')
+contexts, target  = create_contexts_target(corpus, window_size=1)
+print(contexts)
+print(target)
+print('\n')
+
+print('===== Convert to One-Hot Vector =====')
+vocab_size = len(word_to_id)
+target = convert_one_hot(target, vocab_size=vocab_size)
+contexts = convert_one_hot(contexts, vocab_size=vocab_size)
+print(contexts)
+print(target)
