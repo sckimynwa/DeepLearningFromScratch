539e8e6762b1ff69e934d55e2bcc4a0a3a781875
sckimynwa <tsb03672@naver.com>
2021-11-07 22:22:20 +0900

    feat: add embedding layer

--- Book2/common/layers.py
+++ Book2/common/layers.py
@@ -299,3 +299,24 @@ class Pooling:
         dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
         
         return dx
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+    
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+    
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+        
+        for i, word_id in enumerate(self.idx):
+            dW[word_id] += dout[i]
+        
+        return None
